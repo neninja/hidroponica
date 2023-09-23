25218ada61eed4961cddc30e3ca3b4d18d5a93ae
Neni <felipe.dacs@gmail.com>
2023-09-23 18:40:50 -0300

    :new: helps hidroponiqa

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -25,7 +25,9 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            if (app()->environment('production')) {
+                return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            }
         });
 
         $this->routes(function () {

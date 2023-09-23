8f87b4c6cd76d235ebf8dc32ed8ec4fbd5f373eb
Neni <felipe.dacs@gmail.com>
2023-09-23 04:03:40 -0300

    :new: i18n

---
+++ lang/pt_BR/errors.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'not_found' => 'Não Encontrado',
+    'generic' => 'Erro genérico',
+    'rate_limit_exceeded' => 'Limite de requisições',
+    'unauthorized' => 'Não autorizado',
+    'invalid_credential' => 'Credenciais inválidas',
+    'method_not_allowed' => 'Método não permitido',
+    'not_found' => 'Não encontrado',
+];

8cbafa30e5601b0aa9a9fb0b30a825381bf1c226
Neni <felipe.dacs@gmail.com>
2024-01-15 23:30:26 -0300

    :bug: fix demo seeder

--- database/seeders/DemoSeeder.php
+++ database/seeders/DemoSeeder.php
@@ -7,38 +7,40 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DevSeeder extends Seeder
+class DemoSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    public const USER_ID = '9855f644-24db-44b4-88ca-704100097bb4';
+    public const DEMO_OPERATOR_ID = '9855f644-24db-44b4-88ca-704100097bb4';
+
+    public const DEMO_STUDENT_ID = '60b6c856-1967-40e1-9cdc-b122e4cd0458';
 
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $this->createUser();
+        $this->createUsers();
     }
 
-    public function createUser(): void
+    public function createUsers(): void
     {
-        if (User::withTrashed()->find(self::USER_ID)) {
-            return;
+        if (! User::find(self::DEMO_OPERATOR_ID)) {
+            User::factory()->create([
+                'id' => self::DEMO_OPERATOR_ID,
+                'email' => 'demo.operador@hidroponi.ca',
+                'role' => UserRole::DemoOperator,
+                'password' => '123',
+            ]);
         }
 
-        User::factory()->create([
-            'id' => self::USER_ID,
-            'email' => 'demo.operador@hidroponi.ca',
-            'role' => UserRole::DemoOperator,
-            'password' => '123',
-        ]);
-
-        User::factory()->create([
-            'id' => self::USER_ID,
-            'email' => 'demo.aluno@hidroponi.ca',
-            'role' => UserRole::DemoStudent,
-            'password' => '123',
-        ]);
+        if (! User::find(self::DEMO_STUDENT_ID)) {
+            User::factory()->create([
+                'id' => self::DEMO_STUDENT_ID,
+                'email' => 'demo.aluno@hidroponi.ca',
+                'role' => UserRole::DemoStudent,
+                'password' => '123',
+            ]);
+        }
     }
 }
